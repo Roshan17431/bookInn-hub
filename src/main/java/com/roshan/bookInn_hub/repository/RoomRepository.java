fd545393a87adafc7fd68f80750e79a49f02cc71
Roshan17431 <roshanmathew414@gmail.com>
2026-01-08 06:16:42 +0530

    Add RoomRepository interface for room management queries

---
+++ src/main/java/com/roshan/bookInn_hub/repository/RoomRepository.java
@@ -0,0 +1,27 @@
+package com.roshan.bookInn_hub.repository;
+
+import com.roshan.bookInn_hub.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    @Query("SELECT DISTINCT r.roomType FROM Room r")
+    List<String> findDistinctRoomType();
+
+    @Query("SELECT r FROM Room r WHERE r.id NOT IN (SELECT b.room.id FROM Booking b)")
+    List<Room> getAllAvailableRooms();
+
+    @Query("SELECT r FROM Room r WHERE r.roomType LIKE %:roomType% AND r.id NOT IN " +
+            "(SELECT bk.room.id FROM Booking bk WHERE" +
+            "(bk.checkInDate <= :checkOutDate AND (bk.checkOutDate >= :checkInDate)))")
+    List<Room> findAllAvailableRoomsByDatesAndTypes(LocalDate checkInDate,
+                                                    LocalDate checkOutDate,
+                                                    String roomType
+    );
+}
