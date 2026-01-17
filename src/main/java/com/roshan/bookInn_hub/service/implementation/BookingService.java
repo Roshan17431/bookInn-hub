cd50893288a0dd2ff74d080f72585ad18ee1b4af
Roshan17431 <roshanmathew414@gmail.com>
2026-01-17 22:25:59 +0530

    Fix exception handling in saveBooking method to properly catch and respond to errors

--- src/main/java/com/roshan/bookInn_hub/service/implementation/BookingService.java
+++ src/main/java/com/roshan/bookInn_hub/service/implementation/BookingService.java
@@ -48,8 +48,13 @@ public class BookingService implements IBookingService {
             response.setMessage("Booking Successful");
             response.setBookingConfirmationCode(bookingConfirmationCode);;
         }
-        catch{
-
+        catch(OurException e){
+            response.setStatusCode(400);
+            response.setMessage(e.getMessage());
+        }
+        catch(Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error Saving a booking: " + e.getMessage());
         }
     }
 }
