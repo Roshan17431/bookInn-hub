d6c3d8e38989f78f3e6c2df8a04f57964e26003b
Roshan17431 <roshanmathew414@gmail.com>
2026-01-21 23:26:22 +0530

    Implement RoomService class with addNewRoom method for adding new rooms and handling image uploads

--- src/main/java/com/roshan/bookInn_hub/service/implementation/RoomService.java
+++ src/main/java/com/roshan/bookInn_hub/service/implementation/RoomService.java
@@ -1,4 +1,48 @@
 package com.roshan.bookInn_hub.service.implementation;
 
-public class RoomService {
+import com.roshan.bookInn_hub.dto.Response;
+import com.roshan.bookInn_hub.dto.RoomDTO;
+import com.roshan.bookInn_hub.entity.Room;
+import com.roshan.bookInn_hub.repository.RoomRepository;
+import com.roshan.bookInn_hub.security.Utils;
+import com.roshan.bookInn_hub.service.interfac.IRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class RoomService implements IRoomService {
+
+    private final RoomRepository roomRepository;
+    private final AwsS3Service awsS3Service;
+
+    @Override
+    public Response addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice,String description) {
+
+        Response response = new Response();
+        try{
+            Room room = new Room();
+            String imageUrl = awsS3Service.saveImageToS3(photo);
+            room.setRoomPhotoUrl(imageUrl);
+            room.setRoomType(roomType);
+            room.setRoomDescription(description);
+            room.setRoomPrice(roomPrice);
+
+            Room savedRoom = roomRepository.save(room);
+            RoomDTO dto = Utils.mapRoomEntityToRoomDTO(savedRoom);
+
+            response.setStatusCode(200);
+            response.setMessage("Successful");
+            response.setRoom(dto);
+        }
+        catch(Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error Occurred while adding room " + e.getMessage());
+        }
+        return response;
+
+    }
 }
