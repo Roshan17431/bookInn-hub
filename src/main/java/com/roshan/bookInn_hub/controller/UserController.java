5524c5fdc7106332f11883f52203e32eaa9965b7
Roshan17431 <roshanmathew414@gmail.com>
2026-02-09 06:11:05 +0530

    Add UserController with getAllUsers endpoint for admin access

---
+++ src/main/java/com/roshan/bookInn_hub/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.roshan.bookInn_hub.controller;
+
+
+import com.roshan.bookInn_hub.dto.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.roshan.bookInn_hub.service.interfac.IUserService;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final IUserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllUsers(){
+        Response response = userService.getAllUsers();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
