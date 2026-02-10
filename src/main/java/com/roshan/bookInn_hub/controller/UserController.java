344e3a88103256972fe4bf559fff76d50c709817
Roshan17431 <roshanmathew414@gmail.com>
2026-02-10 22:27:30 +0530

    Add getUserById endpoint to UserController for fetching user by ID

--- src/main/java/com/roshan/bookInn_hub/controller/UserController.java
+++ src/main/java/com/roshan/bookInn_hub/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.roshan.bookInn_hub.service.interfac.IUserService;
@@ -21,6 +22,12 @@ public class UserController {
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Response> getAllUsers(){
         Response response = userService.getAllUsers();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/get-by-id/{userId}")
+    public ResponseEntity<Response> getAllUserById(@PathVariable("userId") String userId){
+        Response response = userService.getUserById(userId);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 }
