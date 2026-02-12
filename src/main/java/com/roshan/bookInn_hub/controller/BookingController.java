55e549201dc2733033c04c088a738c7c41a21209
Roshan17431 <roshanmathew414@gmail.com>
2026-02-12 22:11:06 +0530

    Add getAllBookings endpoint to BookingController for fetching all bookings

--- src/main/java/com/roshan/bookInn_hub/controller/BookingController.java
+++ src/main/java/com/roshan/bookInn_hub/controller/BookingController.java
@@ -22,8 +22,15 @@ public class BookingController {
             @PathVariable Long roomId,
             @PathVariable Long userId,
             @RequestBody Booking bookingRequest){
-        
+
         Response response = bookingService.saveBooking(roomId, userId, bookingRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllBookings(){
+        Response response = bookingService.getAllBookings();
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
