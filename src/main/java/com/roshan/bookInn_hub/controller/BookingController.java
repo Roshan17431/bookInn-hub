65a74a534afcd9510557d21a39a25afe80c992e4
Roshan17431 <roshanmathew414@gmail.com>
2026-02-12 08:17:46 +0530

    Add BookingController with saveBookings endpoint for room booking

---
+++ src/main/java/com/roshan/bookInn_hub/controller/BookingController.java
@@ -0,0 +1,31 @@
+package com.roshan.bookInn_hub.controller;
+
+
+import com.roshan.bookInn_hub.dto.Response;
+import com.roshan.bookInn_hub.entity.Booking;
+import com.roshan.bookInn_hub.service.interfac.IBookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bookings")
+@RequiredArgsConstructor
+public class BookingController {
+
+    private final IBookingService bookingService;
+
+    @PostMapping("/book-room/{roomId}/{userId}")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
+    public ResponseEntity<Response> saveBookings(
+            @PathVariable Long roomId,
+            @PathVariable Long userId,
+            @RequestBody Booking bookingRequest){
+        
+        Response response = bookingService.saveBooking(roomId, userId, bookingRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+
+}
