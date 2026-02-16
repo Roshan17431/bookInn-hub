9b506a7f6638e57ca98f7a4d528dc42788ce6de0
Roshan17431 <roshanmathew414@gmail.com>
2026-02-16 22:36:44 +0530

    Refactor RoomController imports and clean up code structure

--- src/main/java/com/roshan/bookInn_hub/controller/RoomController.java
+++ src/main/java/com/roshan/bookInn_hub/controller/RoomController.java
@@ -5,10 +5,7 @@ import com.roshan.bookInn_hub.service.interfac.IRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
@@ -38,5 +35,10 @@ public class RoomController {
         Response response = roomService.addNewRoom(photo, roomType, roomPrice, roomDescription);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
-    
+
+    @GetMapping("/all")
+    public ResponseEntity<Response> getAllUsers(){
+        Response response = roomService.getAllRooms();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
 }
