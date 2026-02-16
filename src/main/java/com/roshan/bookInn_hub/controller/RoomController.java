a41be1bb21b7733933b0a4271b951e69964b5cdb
Roshan17431 <roshanmathew414@gmail.com>
2026-02-16 22:37:17 +0530

    Add getRoomTypes endpoint to RoomController for fetching available room types

--- src/main/java/com/roshan/bookInn_hub/controller/RoomController.java
+++ src/main/java/com/roshan/bookInn_hub/controller/RoomController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rooms")
@@ -40,5 +41,10 @@ public class RoomController {
     public ResponseEntity<Response> getAllUsers(){
         Response response = roomService.getAllRooms();
         return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/types")
+    public List<String> getRoomTypes() {
+        return roomService.getAllRoomTypes();
     }
 }
